2d32ca0f474b20641bd984c4c87a9349f6914f37
Thalles Neves <thalles.futurotec.com.br>
2023-12-09 17:52:19 -0300

    Frontend : Feat : Alteração do nome da aplicação Alteração do ícone da aplicação Criação de sistema para manter o login ao recarregar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,10 +6,16 @@ import AuthPage from "./AuthPage";
 import ChatsPage from "./ChatsPage";
 
 function App() {
-  const [user, setUser] = useState(undefined);
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")));
+
+  function onAuth(user) {
+    localStorage.setItem("user", JSON.stringify(user));
+
+    setUser(user);
+  }
 
   if (!user) {
-    return <AuthPage onAuth={(user) => setUser(user)} />;
+    return <AuthPage onAuth={(user) => onAuth(user)} />;
   } else {
     return <ChatsPage user={user} />;
   }
